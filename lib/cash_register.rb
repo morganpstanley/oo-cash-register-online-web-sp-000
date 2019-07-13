dec5f819dae98b66c78359e65de9ba71e5a442e3
Morgan Stanley <morganpstanley@gmail.com>
2019-07-13 18:20:19 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,18 @@
 
 class CashRegister
   attr_accessor :total
+  
+#_____________INITIALIZE______________________
 
     def initialize(discount = 0)
       @total = 0
       @items = []
       @discount = discount
     end
+#_____________ADD ITEMS_____________________
 
   def add_item(item, price, quantity = 1)
+    @price = price.to_f
     i = 0
     until i == quantity
       @items << item
@@ -16,9 +20,15 @@ class CashRegister
     end
     @total += price * quantity
   end
+#_________________ITEMS____________________
 
   def items
     @items
   end
+#__________________________________________
+
+  def apply_discount
+    @price 
+    
 
 end
