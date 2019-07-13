35ffb11190d0449b347b61081c9b3807b4c7502a
Morgan Stanley <morganpstanley@gmail.com>
2019-07-13 18:44:33 +0000

    Automatically backed up by Learn

---
+++ a.rb
@@ -0,0 +1,48 @@
+
+class CashRegister
+  attr_accessor :total
+
+#_____________INITIALIZE______________________
+
+    def initialize(discount = 0)
+      @total = 0
+      @items = []
+      @discount = discount
+    end
+#_____________ADD_ITEMS_____________________
+
+  def add_item(item, price, quantity = 1)
+    @price = price.to_f
+    i = 0
+    until i == quantity
+      @items << item
+      i += 1
+    end
+    @total += price * quantity
+  end
+#_________________ITEMS____________________
+
+  def items
+    @items
+  end
+#____________APPLY_DISCOUNTS_______________
+
+  def apply_discount
+    if @discount == 0
+      puts "There is no discount to apply"
+    else
+      @price * 0.2
+      @total -= @price
+      puts "After the discount, the total comes to #{@total}."
+    end
+  end
+
+#______________DISCOUNT____________________
+
+  def discount
+    @discount
+  end
+
+
+
+end
