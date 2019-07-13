26fd27642602476b12065fec38cba78a317acf01
Morgan Stanley <morganpstanley@gmail.com>
2019-07-13 17:45:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
 
+class CashRegister
+    @@total = 0
+    @@items = []
+
+  def total
+    @@total
+  end
+
+  def add_item(item, price, quantity = "1")
+    i = 0
+    until i == quantity
+      @@items << item
+      i += 1
+    end
+    @@total += price * quantity
+  end
+
+  def items
+    @@items
+  end
+
+end
