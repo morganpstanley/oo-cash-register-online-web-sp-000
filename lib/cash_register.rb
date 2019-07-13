88fc34ac204a462c7dcc4153f4b016f55adac5b4
Morgan Stanley <morganpstanley@gmail.com>
2019-07-13 19:04:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -29,11 +29,11 @@ class CashRegister
 
   def apply_discount
     if @discount == 0
-      return "There is no discount to apply"
+      return "There is no discount to apply."
     else
       @price *= 0.2
       @total -= @price
-      return "After the discount, the total comes to $#{@total}."
+      return "After the discount, the total comes to $#{@total.to_i}."
     end
   end
 
